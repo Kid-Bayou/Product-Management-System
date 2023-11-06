22e6976a14d0ff3cecec2af8b4a1ab3f9f5dbd3f
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-11-06 16:14:43 +0300

    .

--- app/src/pages/DeleteProduct.jsx
+++ app/src/pages/DeleteProduct.jsx
@@ -5,7 +5,7 @@ function DeleteProducts({ closeModal, product }) {
     <>
       <div className="modal-background">
         <div className="modal-container">
-          <p>Are you sure you want to delete this product?</p>
+          <p className="confirmation-text">Are you sure you want to delete this product?</p>
           <div className="delete-btn-container">
             <button className="delete-btn">Yes</button>
             <button className="delete-btn" onClick={() => closeModal(false)}>No</button>
